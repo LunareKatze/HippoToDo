83ccb864f46a43596a0b97b57224da325ce7a101
LunareKatze <lunarekatze17@gmail.com>
2021-03-14 23:36:21 +0300

    Remove unused code

--- app/src/main/java/com/lunarekatze/hippotodo/MainActivity.java
+++ app/src/main/java/com/lunarekatze/hippotodo/MainActivity.java
@@ -3,8 +3,6 @@ package com.lunarekatze.hippotodo;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -77,26 +75,14 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.ItemCli
             taskList.add(cursor.getString(index));  // Получаем строку по индексу и пишем её в ArrayList
             int indexStatus = cursor.getColumnIndex(TaskHelper.COLUMN_STATUS);  // Получаем индекс для каждого элемента в базе в столбце COLUMN_STATUS
             statusList.add(cursor.getString(indexStatus));  // Получаем строку по индексу и пишем её в ArrayList
-
-            //Cursor cursorStatus = db.rawQuery("SELECT * FROM " + TaskHelper.DATABASE_TABLE + " WHERE " + TaskHelper.COLUMN_STATUS + " = '" + "done" + "'", null);   //ищем в базе done задачи
-            //int countStatus = cursor.getCount();      //пишем в int номер строки done задачи
         }
         if(adapter == null) {                   // если адаптер пустой
             adapter = new MyAdapter(this, taskList, statusList);        // Передаём в адаптер ArrayList
             adapter.setClickListener(this);
             recyclerView.setAdapter(adapter);
-            //Cursor cursorStatus = db.rawQuery("SELECT * FROM " + TaskHelper.DATABASE_TABLE + " WHERE " + TaskHelper.COLUMN_STATUS + " = '" + "done" + "'", null);   //ищем в базе done задачи
-            //int countStatus = cursor.getCount();      //пишем в int номер строки done задачи
-/*                if(countStatus != 0){
-                    DividerItemDecoration itemDecorationDone = new DividerItemDecoration(recyclerView.getContext(), layoutManager.getOrientation());
-                    itemDecorationDone.setDrawable(new ColorDrawable(Color.rgb(128,128,128)));
-                    recyclerView.addItemDecoration(itemDecorationDone);*/
-
             DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),              // линии
                     layoutManager.getOrientation());
             recyclerView.addItemDecoration(dividerItemDecoration);
-
-            //changeColorTasks(View view);
         }
         else {
             adapter.clear();
@@ -107,64 +93,21 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.ItemCli
         db.close();
     }
 
-    /*public void changeColorTasks (ArrayList tasks, View view) {
-        //ArrayList <String> tasks = new ArrayList<>();
-        String status;
-        SQLiteDatabase db = mHelper.getWritableDatabase();
-        Cursor cursor = db.query(TaskHelper.DATABASE_TABLE,
-                new String[]{TaskHelper.COLUMN_ID, TaskHelper.COLUMN_NAME, TaskHelper.COLUMN_STATUS}, null, null, null, null, null);
-        while (cursor.moveToNext()) {    // Пока у курсора есть следующий элемент...
-            int index = cursor.getColumnIndex(TaskHelper.COLUMN_STATUS);  // Получаем индекс для каждого элемента в базе в столбце COLUMN_STATUS
-            status = cursor.getString(index);  // Получаем строку по индексу и пишем её в ArrayList
-                if (status.equals("done")) {
-                    //final RecyclerView.ViewHolder holder;
-                    //holder = new RecyclerView.ViewHolder(convertView);
-                    //MyAdapter.MyViewHolder.class.
-                    //super(itemView);
-                    //textView = itemView.findViewById(R.id.task_title)
-                    //adapter.getItem(findViewById(R.id.task_title))
-                    View parent = (View) view.getParent();
-                    TextView taskTextView = parent.findViewById(R.id.task_title);
-                    taskTextView.setTextColor(Color.rgb(128,128,128));
-            }
-            else {
-                break;
-            }
-        }
-    }*/
-
     public void changeStatusTask(View view) {
         View parent = (View) view.getParent();
         TextView taskTextView = parent.findViewById(R.id.task_title);
-        //TextView test_text = parent.findViewById(R.id.test_text);
-        //String string = adapter.getItem(position);
         CheckBox checkBox = parent.findViewById(R.id.change_status);
-
         ContentValues values = new ContentValues();         // Класс исп-ся для добавления новых строк в таблицу
-
         if (checkBox.isChecked()){
-//            checkBox.setChecked(false);
-//            test_text.setText("false");
             //Toast.makeText(getApplicationContext(), "true", Toast.LENGTH_LONG).show();
             values.put("status", "done");
         }
         else {
-//            checkBox.setChecked(true);
-//            test_text.setText("true");
             //Toast.makeText(getApplicationContext(), "false", Toast.LENGTH_LONG).show();
             values.put("status", "current");
         }
-
-        //checkBox.setChecked(false);
-        //String task = String.valueOf(taskTextView.getText());
-
-
         SQLiteDatabase db = mHelper.getWritableDatabase();
-        //db.delete(TaskHelper.DATABASE_TABLE, TaskHelper.COLUMN_NAME + " =?", new String[] {task});
-        //db.update(TaskHelper.COLUMN_NAME, values, TaskHelper.COLUMN_ID + "=" + String.valueOf(id), null);
         db.update(TaskHelper.DATABASE_TABLE, values, TaskHelper.COLUMN_NAME + "= ?", new String[] { taskTextView.getText().toString() });
-        //TaskHelper.COLUMN_NAME + " = '" + task + "'"
-        //values.put(TaskHelper.COLUMN_STATUS, "done");
         db.close();
         updateUI();
     }
@@ -172,8 +115,6 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.ItemCli
     public void deleteTask(View view) {
         View parent = (View) view.getParent();
         TextView taskTextView = parent.findViewById(R.id.task_title);       //пишем в переменную текст задачи
-        //CheckBox checkBox = parent.findViewById(R.id.delete_task);
-        //checkBox.setChecked(false);
         String task = String.valueOf(taskTextView.getText());
         SQLiteDatabase db = mHelper.getWritableDatabase();
         db.delete(TaskHelper.DATABASE_TABLE, TaskHelper.COLUMN_NAME + " =?", new String[] {task});
